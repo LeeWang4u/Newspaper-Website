dd6b15a80852887717129eeb497cfefc0aa65702
lequanghien4u@gmail.com <lequanghien4u@gmail.com>
2024-06-06 00:50:50 +0700

    Add verification email code  when registering

--- src/main/java/com/ptit/controller/login/SignUpController.java
+++ src/main/java/com/ptit/controller/login/SignUpController.java
@@ -1,16 +1,24 @@
 package com.ptit.controller.login;
 
 import com.ptit.Dto.UserDto;
+import com.ptit.Service.MailerService;
 import com.ptit.Service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 @AllArgsConstructor
 public class SignUpController {
+    @Autowired
+    private MailerService mailerService;
     private UserService userService;
 
     @ModelAttribute("userdto")
@@ -24,11 +32,38 @@ public class SignUpController {
     }
 
     @PostMapping("/signUp")
-    public String signUpUserAccount(@ModelAttribute("userdto") UserDto userDto){
+    public String signUpUserAccount(@ModelAttribute("userdto") UserDto userDto, HttpSession session){
         if(userService.checkUserByEmail(userDto.getEmail())){
             return "redirect:/signUp?emailexist";
         }
-        userService.save(userDto);
-        return "redirect:/login?success";
+
+        Random rand = new Random();
+        int randomNum = 1000 + rand.nextInt(9000);
+        String codeVerify = String.valueOf(randomNum);
+
+        session.setAttribute("userdto", userDto);
+        session.setAttribute("codeVerify", codeVerify);
+        mailerService.send( "New24h-noreply", userDto.getEmail().trim(), "Mã xác nhân từ New24h",mailerService.bodyReply(codeVerify));
+
+        return "redirect:/verify-signup";
+    }
+
+    @GetMapping("/verify-signup")
+    public String enterVerifySignUp(){
+        return "admin/verify";
+    }
+
+    @PostMapping("/verify-signup")
+    public String VerifySignUp(HttpSession session, @RequestParam("code") String code){
+        UserDto userDto = (UserDto) session.getAttribute("userdto");
+        String codeVerify = (String)session.getAttribute("codeVerify");
+
+        if(code.equals(codeVerify)){
+            userService.save(userDto);
+            return "redirect:/login?success";
+        }
+
+        return "redirect:/signUp?emailexist";
+
     }
 }
